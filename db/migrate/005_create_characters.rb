0fcb6962220db5af7edc810040731ab0d8ce6b00
katiethile <lethikatie@gmail.com>
2020-09-17 11:31:41 -0700

    Done.

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,8 +1,7 @@
-class CreateCharacters < ActiveRecord::Migration[5.1]
+class CreateCharacters < ActiveRecord::Migration[5.2]
     def change
         create_table :characters do |c|
           c.string :name
-          c.integer :actor_id
           c.integer :show_id
           c.string :catchphrase
         end
